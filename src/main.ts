a0daaa024ad74d31b6c8675327e58d9006f69bbb
Arjun Prajapat <86150031+arjun-prajapat@users.noreply.github.com>
2026-04-30 17:23:39 +0530

    Add CORS origin for Vercel frontend

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,10 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: ["http://localhost:3000"],
+    origin: [
+              "http://localhost:3000",
+              "https://frontend-blog-app-seven.vercel.app",
+            ],
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   });
