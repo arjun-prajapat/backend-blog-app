e551439246bd9b483339f593aad3069da98d4d85
Arjun <introarjun@gmail.com>
2026-04-30 11:02:26 +0530

    Update cors details

--- src/main.ts
+++ src/main.ts
@@ -12,10 +12,9 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: [
-      "http://localhost:3000",
-    ],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   });
   await app.listen(process.env.PORT ?? 3000);
 }
